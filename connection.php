0d0f0dd5bd99d6e75cdec3037f388b5bf4f8ae97
thatipamulanaresh <thatipamulanaresh123@gmail.com>
2023-10-30 11:25:12 +0530

    uploadimage

---
+++ connection.php
@@ -0,0 +1,41 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+//$password = "TeamUp!306";
+$password = "";
+$db="test";
+
+
+$con = new mysqli($servername, $username, $password, $db);
+
+
+if($con->connect_error)
+{
+    die("connection failed");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
